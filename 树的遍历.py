70e35891f5249d204d8b31465d3e846cd4483918
Manquan Fang <42682039+mf734@users.noreply.github.com>
2019-10-16 21:30:52 -0400

    Add files via upload

--- 树的遍历.py
+++ 树的遍历.py
@@ -1,9 +1,9 @@
 # 1. 中序遍历
 # 1.1 递归实现中序遍历
 def inorderT(self, root):
-	if not root:
-		return []
-	return self.inorderT(root.left) + [root.val] + self.inorderT(root.right)
+    if not root:
+        return []
+    return self.inorderT(root.left) + [root.val] + self.inorderT(root.right)
 
 
 
@@ -13,33 +13,33 @@ def inorderT(self, root):
 # 当p none了，说明刚刚遍历到左子结点
 # 现在需要回溯到上一步，然后走上一步的右子树
 def inorderT2(self, root):
-	stack = []
-	res = []
-	p = root
-	while stack or p:
-		if p:
-			stack.append(p)
-			p = p.left
-		else:
-			p = stack.pop()
-			res.append(p.val)
-			p = p.right
-	return res
+    stack = []
+    res = []
+    p = root
+    while stack or p:
+        if p:
+            stack.append(p)
+            p = p.left
+        else:
+            p = stack.pop()
+            res.append(p.val)
+            p = p.right
+    return res
 
 
 
 # 2. 前序遍历和后序遍历
 # 2.1.1 递归实现前序遍历
 def preorderT(self, root):
-	if not root:
-		return []
-	return [root.val] + self.preorderT(root.left) + self.preorderT(root.right)
+    if not root:
+        return []
+    return [root.val] + self.preorderT(root.left) + self.preorderT(root.right)
 
 # 2.1.2 递归实现后序遍历
 def posorderT(self, root):
-	if not root:
-		return []
-	return self.posorderT(root.left) + self.posorderT(root.right) + [root.val]
+    if not root:
+        return []
+    return self.posorderT(root.left) + self.posorderT(root.right) + [root.val]
 
 # 2.2.1 循环实现前序遍历
 # 立即打印当前结点，并push右子结点入栈
@@ -47,32 +47,32 @@ def posorderT(self, root):
 # 当前结点非none，每一次循环将当前结点入栈
 # 当前结点为none，则出栈一个结点
 def preorderT2(self, root):
-	stack = []
-	res = []
-	p = root
-	while stack or p:
-		if p:
-			res.append(p.val)
-			stack.append(p.right)
-			p = p.left
-		else:
-			p = stack.pop()
-	return res
+    stack = []
+    res = []
+    p = root
+    while stack or p:
+        if p:
+            res.append(p.val)
+            stack.append(p.right)
+            p = p.left
+        else:
+            p = stack.pop()
+    return res
 
 # 2.2.2 循环实现后序遍历
 # 原理和前序很近
 def posOrderT2(self, root):
-	stack = []
-	res = []
-	p = root
-	while stack or p:
-		if p:
-			res.append(p.val)
-			stack.append(p.left)
-			p = p.right
-		else:
-			p = stack.pop()
-	return res[::-1]
+    stack = []
+    res = []
+    p = root
+    while stack or p:
+        if p:
+            res.append(p.val)
+            stack.append(p.left)
+            p = p.right
+        else:
+            p = stack.pop()
+    return res[::-1]
 
 
 
@@ -80,21 +80,21 @@ def posOrderT2(self, root):
 # 3.1 递归实现 (从左到右）
 # 递归函数有一个level，表示结点当前的层数
 def levelOrder(self, root):
-	def helper(node, level):
-		if not node:
-			return
-		else:
-			res[level-1].append(node.val)
-			# 遍历到新层时，只有最左边的结点使得等式成立
-			# 也就是新一层的第一个结点
-			# 建立空的，为下一层做准备
-			if len(res) == level:
-				res.append([])
-			helper(node.left, level+1)
-			helper(node.right, level+1)
-	res = [[]]
-	helper(root, 1)
-	return res[::-1]
+    def helper(node, level):
+        if not node:
+            return
+        else:
+            res[level-1].append(node.val)
+            # 遍历到新层时，只有最左边的结点使得等式成立
+            # 也就是新一层的第一个结点
+            # 建立空的，为下一层做准备
+            if len(res) == level:
+                res.append([])
+            helper(node.left, level+1)
+            helper(node.right, level+1)
+    res = [[]]
+    helper(root, 1)
+    return res[:-1]
 
 # 如果是从右到左，只需要互换left和right就行
 # 这样就会以right的第一个为以上if的判断点
@@ -107,36 +107,39 @@ def levelOrder(self, root):
 # 每一层的顺序一样
 # 所以FIFO
 def levelOrder2(self, root):
-	if not root:
-		return []
-	res = []
-	p = root
-	queue = [p]
-	while queue:
-		p = queue.pop(0)
-		res.append(p.val)
-		if p.left:
-			queue.append(p.left)
-		if p.right:
-			queue.append(p.right)
-	return res
+    if not root:
+        return []
+    res = []
+    queue = [root]
+    while queue:
+        tmp = []
+        next_level = []
+        for i in queue: 
+            tmp.append(i.val)
+            if i.left:
+                next_level.append(i.left)
+            if i.right:
+                next_level.append(i.right)
+        res.append(tmp)
+        queue = next_level
+    return res
 
 
 
 # 4.二叉树结点个数
 def treeNode(self, root):
-	if not root:
-		return 0
-	nums = treeNode(root.left)
-	nums += treeNode(root.right)
-	return nums + 1
+    if not root:
+        return 0
+    nums = treeNode(root.left)
+    nums += treeNode(root.right)
+    return nums + 1
 
 
 
 # 5.二叉树的最大深度
 def TreeDepth(self, root):
-	if not root:
-		return 0
-	ld = TreeDepth(root.left)
-	rd = TreeDepth(root.right)
-	return (max(ld, rd) + 1)
+    if not root:
+        return 0
+    ld = TreeDepth(root.left)
+    rd = TreeDepth(root.right)
+    return (max(ld, rd) + 1)
